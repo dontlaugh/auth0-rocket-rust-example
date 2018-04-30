c454e1dbb2590765d52c7235f9c8c4bf73efb7fc
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-04-30 11:32:18 -0700

    use result type for config errors

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Oauth with Rocket against Auth0
 
 https://auth0.com/docs/application-auth/current/server-side-web
+
+### TODO
+
+* put state string in cookie before redirect to login page
+* verify state param in callback
+* put session into cookie (hash something from the jwt)
+
--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,32 @@
 #![feature(custom_derive)]
 
 extern crate maud;
+extern crate reqwest;
 extern crate rocket;
 extern crate rocket_codegen;
-extern crate reqwest;
 extern crate url;
 
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 extern crate serde;
+extern crate serde_json;
 
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex, RwLock};
-use std::path::{Path, PathBuf};
 use serde::Serialize;
 use serde_json::ser::to_vec;
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex, RwLock};
 
 use maud::{html, Markup};
-use rocket::response::status;
-use rocket::request::{FromRequest, Outcome, Request};
-use rocket::State;
-use rocket::http::Status;
-use rocket::response::Redirect;
 use rocket::fairing::AdHoc;
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::response::status;
+use rocket::response::Redirect;
+use rocket::State;
 
-use url::Url;
 use rocket::http::uri::URI;
+use url::Url;
 
 use std::str::FromStr;
 
@@ -42,12 +42,8 @@ fn main() {
         .mount("/", routes)
         .manage(sessions)
         .attach(AdHoc::on_attach(|rocket| {
-            let app_settings = AppSettings {
-                client_id: String::from(rocket.config().get_str("client_id").unwrap()),
-                client_secret: String::from(rocket.config().get_str("client_secret").unwrap()),
-                redirect_uri: String::from(rocket.config().get_str("redirect_uri").unwrap()),
-                auth0_domain: String::from(rocket.config().get_str("auth0_domain").unwrap()),
-            };
+            let conf = rocket.config().clone();
+            let app_settings = AppSettings::from_rocket_config(&conf).expect("configuration error");
             Ok(rocket.manage(app_settings))
         }))
         .launch();
@@ -98,8 +94,6 @@ fn login(settings: State<AppSettings>) -> Redirect {
     Redirect::to(&data.to_url())
 }
 
-
-
 /// Login callback. Auth0 sends a request to this endpoint. In this function,
 /// we extract the "code" and "state" parameters, ensuring that state matches
 /// exactly the string we passed to Auth0's /authorize endpoint. Then we can
@@ -117,19 +111,24 @@ fn login_code(code: CodeAndState, settings: State<AppSettings>) -> Result<String
     };
     // TODO check state
     //let resp = reqwest::get(&data.to_url()).expect("get failed to token endpoint");
-    
+
     //let encoded_url = &URI::percent_encode(&data.redirect_uri).to_string();
     let mut params = HashMap::new();
-    params.insert("grant_type", "authorization_code");
-    params.insert("code", code.code.as_str());
-    params.insert("redirect_uri", &data.redirect_uri);
-    params.insert("client_id", &data.client_id);
-    params.insert("client_secret", &data.client_secret);
+    {
+        params.insert("grant_type", "authorization_code");
+        params.insert("code", code.code.as_str());
+        params.insert("redirect_uri", &data.redirect_uri);
+        params.insert("client_id", &data.client_id);
+        params.insert("client_secret", &data.client_secret);
+    }
     println!("params! {:?}", params);
     let token_url = "https://coleman.auth0.com/oauth/token";
     let resp = reqwest::Client::new()
-        .post(token_url.clone()).header(ContentType(reqwest::mime::APPLICATION_JSON))
-        .body(to_vec(&params).expect("could not serialize hashmap")).send().expect("POST REQUEST");
+        .post(token_url.clone())
+        .header(ContentType(reqwest::mime::APPLICATION_JSON))
+        .body(to_vec(&params).expect("could not serialize hashmap"))
+        .send()
+        .expect("POST REQUEST");
     println!("got response: {:?}", resp);
 
     Ok(String::from("Thanks"))
@@ -181,6 +180,20 @@ struct AppSettings {
     client_secret: String,
     redirect_uri: String,
     auth0_domain: String,
+}
+
+impl AppSettings {
+    pub fn from_rocket_config(
+        conf: &rocket::Config,
+    ) -> Result<AppSettings, rocket::config::ConfigError> {
+        let app_settings = AppSettings {
+            client_id: String::from(conf.get_str("client_id")?),
+            client_secret: String::from(conf.get_str("client_secret")?),
+            redirect_uri: String::from(conf.get_str("redirect_uri")?),
+            auth0_domain: String::from(conf.get_str("auth0_domain")?),
+        };
+        Ok(app_settings)
+    }
 }
 
 #[derive(Debug)]
